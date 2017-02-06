0e69b5213ed89180216cc747932a22dbba193b00
Youcef Bennour <ybennour@uw.edu>
2017-02-06 11:56:22 -0800

    Update Job.java

--- src/model/Job.java
+++ src/model/Job.java
@@ -5,23 +5,28 @@ package model;
 
 import java.util.List;
 import java.io.Serializable;
+import java.time.LocalTime;
+import java.time.ZoneId;
 
 /**
  * @author Jasmine Dacones
  *
  */
 public class Job implements Serializable {
+	private static final long serialVersionUID = 812488421841214L;
+	
 	int MAX_VOLUME = 30;
 	int MAX_LENGTH = 2;
 
-	private String desc;
+	
+	private String jobDescription;
 	private Park park;
 	private List<UserType> volunteersList;
 	private DateAndTime start;
 	private DateAndTime end;
 	
-	public Job(String theDesc, Park thePark, DateAndTime theStart, DateAndTime theEnd) {
-		desc = theDesc;
+	public Job(String theJobDescription, Park thePark, DateAndTime theStart, DateAndTime theEnd) {
+		jobDescription = theJobDescription;
 		park = thePark;
 		start = theStart;
 		end = theEnd;
@@ -40,4 +45,40 @@ public class Job implements Serializable {
 	public List<UserType> getVolunteers() {
 		return volunteersList;
 	}
+	
+	public String getJobDescription(){
+		return jobDescription;
+	}
+	
+	public Park getPark(){
+		return park;
+	}
+	
+	public DateAndTime getStartTime(){
+		return start;
+	}
+	
+	public DateAndTime getEndTime(){
+		return end;
+	}
+	
+	public void setJobDescription(String theJobDescription){
+		jobDescription = theJobDescription;
+	}
+	
+	public void setPark(Park thePark){
+		park = thePark;
+	}
+	
+	public void setStartTime(DateAndTime theStart){
+		start = theStart;
+	}
+	
+	public void setEndTime(DateAndTime theEnd){
+		end = theEnd;
+	}
+	
+	public String toString() {
+        	return ;
+    	} 
  }
