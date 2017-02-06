6df09573bc02a7b29ae2235c65bf50c202aa0982
Jasmine Dacones <jazzyd25@uw.edu>
2017-02-05 22:10:31 -0800

    Created TestJob.java file.

---
+++ src/test/TestJob.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package model;
+
+
+/**
+ * @author Jasmine Dacones
+ *
+ */
+public class TestJob {
+
+	public static void main(String[] args) {
+		Job jobOne = new Job();
+		jobOne.setDesc("Rake the leaves at Point Defiance Park.");	
+	}
+	
+	public
+	
+}
