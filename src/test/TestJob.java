e852c63345769cc7d35cfaabb11232cbd5c03aef
Jasmine Dacones <jasminedacones@gmail.com>
2017-02-06 20:59:00 -0800

    Implemented test case for TestJob.java

--- src/test/TestJob.java
+++ src/test/TestJob.java
@@ -1,11 +1,16 @@
 /**
  * 
  */
-package model;
+package test;
 
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+
+import model.Job;
+import model.Location;
+import model.Park;
+import model.UserType;
 
 
 /**
@@ -13,14 +18,19 @@ import org.junit.Test;
  *
  */
 public class TestJob {
+	
+	private static Job jobOne;
+	private static UserType volunteerOne;
+	private static Location location = new Location("5472", "Tacoma", "WA");
 
 	public static void main(String[] args) {
-		Job jobOne = new Job();	
-		UserType volunteer1 = new UserType("Alyssa");	
+		jobOne = new Job("Rake the leaves.", new Park(location, "Point Defiance Park"));	
+		volunteerOne = UserType.Volunteer;	
 	}
 	
 	@Test
 	public void testAddVolunteer() {
-		assertTrue(jobOne.addVolunteer(volunteer1), getVolunteers().get(0));
+		jobOne.addVolunteer(volunteerOne);
+		assertEquals(UserType.Volunteer, jobOne.getVolunteers().get(0));
 	}	
 }
