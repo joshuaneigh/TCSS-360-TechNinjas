01e6aed251138f7136fc405d8ebaa9da7c19a933
Michael Loundagin <loundm@uw.edu>
2017-02-07 20:38:56 -0800

    Added ViewJobsMenuItem.

---
+++ src/view/menu/items/ViewJobsMenuItem.java
@@ -0,0 +1,29 @@
+package view.menu.items;
+
+import model.Controller;
+import model.UserType;
+import view.TextUI;
+
+/**
+ * Launch view of pending jobs for a Park Manager
+ * @author Michael Loundagin
+ */
+public class ViewJobsMenuItem implements MenuItem {
+
+	@Override
+	public void activate() {
+		final ViewJobsMenu viewJobsMenu = new ViewJobsMenu();
+		TextUI.navigate(viewJobsMenu);
+	}
+
+	@Override
+	public boolean isAllowed(UserType userType) {
+		return (Controller.getLoggedInUserName() == "administrator") || userType == UserType.ParkManager;
+	}
+
+	@Override
+	public String getLabel() {
+		return "View jobs for this park.";
+	}
+
+}
