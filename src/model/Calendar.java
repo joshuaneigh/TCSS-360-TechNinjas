d6326524477b0cb9f400dad40638a0f240c17af2
Michael Loundagin <loundm@uw.edu>
2017-02-08 23:17:18 -0800

    Cleaned and fixed Calendar.
     * Cleaned the code and added comments for readability
     * Fixed code for toString so that it doesn't throw exceptions and
       works properly
     * Added new test case to CalendarTest
     * Removed TestCalender
     * Author tag for John removed since none of his code is left

--- src/model/Calendar.java
+++ src/model/Calendar.java
@@ -1,21 +1,26 @@
-/**
- * 
- */
 package model;
 
-import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+
 import java.util.List;
 import java.util.Objects;
 
 /**
  * A calendar of the upcoming jobs for a month.
- * @author John Bako, Michael Loundagin
+ * @author Michael Loundagin
  */
 public class Calendar {
+
+    /**
+     * The months of a year.
+     */
+    private static final String[] MONTH_NAMES = {"January", "February", "March",
+                                                 "April", "May", "June",
+                                                 "July", "August", "September",
+                                                 "October", "November", "December"};
 
     /**
      * The number of days in a week.
@@ -26,21 +31,31 @@ public class Calendar {
      * The value when a day should not be displayed.
      */
     private static final int NONDISPLAYED_DAY = -1;
-    
+
     /**
-     * The format expected from Job.getStartTime().
+     * The header for any Calendar containing abbreviations of the weeks days.
      */
-    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final String CALENDAR_HEADER = "   Su      M      T      W      T      F      S\n";
+
+    /**
+     * The start of a month header.
+     */
+    private static final String MONTH_HEADER_START = "               [";
+
+    /**
+     * The end of a month header.
+     */
+    private static final String MONTH_HEADER_END = "]\n";
+
+    /**
+     * The filler for a nondisplayed day.
+     */
+    private static final String EMPTY_DAY = "      |";
     
     /**
      * The current date.
      */
-    private LocalDateTime currentDate;
-    
-    /**
-     * Used across helper methods.
-     */
-    private LocalDateTime endDate;
+    private LocalDateTime currentDate = LocalDateTime.now();
 
     /**
      * The list of all jobs.
@@ -68,96 +83,155 @@ public class Calendar {
      * Formats a calendar of jobs for the coming month.
      * @return A formatted calendar of jobs for the coming month
      */
-	public String toString() {
-        final int numOfWeekRows = calculateNumOfWeekRows(currentDate);
-        final int[][] jobsForCalendarDays = createJobCalendar(currentDate, numOfWeekRows);
-        String returnString = "   Su      M      T      W      T      F      S\n";
-        returnString += "               [" + currentDate.getMonth().toString() + "]\n";
-        returnString += "|";
-        final LocalDateTime iterDate = LocalDateTime.now();
-        final Month currentMonth = currentDate.getMonth();
-        boolean nextMonthFlag = false;
-        for (int i = 0; i < numOfWeekRows; i++) {
-        	for (int j = 0; j < DAYS_IN_WEEK; j++) {
-        		//if we've entered the next month before setting the flag, we need to add the next month header
-        		if (!(nextMonthFlag && iterDate.getMonth().equals(currentMonth))) {
-        			for (int k = j; k < DAYS_IN_WEEK; k++) {
-        				returnString += "      |";
-        			}
-        			returnString += "\n               [" + iterDate.getMonth().toString() + "]\n";
-        			returnString += "|";
-        			for (int k = 0; k < j; k++) {
-        				returnString += "      |";
-        			}
-        		}
-        		returnString += " " + String.format("%1$2d", iterDate.getDayOfMonth()) + ":" + jobsForCalendarDays[i][j] + " |";
-        	}
-        	returnString += "\n|";
-        }
-        return returnString;
-	}
-	
-	private int calculateNumOfWeekRows(final LocalDateTime currentDate) {
-		int numOfWeekRows = 0;
-		final LocalDateTime iterDate = LocalDateTime.now().plusDays(1);
-		final int currentDayOfMonth = currentDate.getDayOfMonth();
-        while (iterDate.getDayOfMonth() != currentDayOfMonth) {
-        	if (iterDate.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
-        		numOfWeekRows++;
-        	}
-        	iterDate.plusDays(1);
-        }
-        endDate = iterDate;
-        return numOfWeekRows;
-	}
-	
-	private int[][] createJobCalendar(final LocalDateTime currentDate, final int numOfWeekRows) {
-        //create array to store number of jobs for each date
-		final int[][] jobsForCalendarDays = new int[numOfWeekRows][DAYS_IN_WEEK];
-        //iterate over the jobs to add the appropriate days
-		final int currentDayOfMonth = currentDate.getDayOfMonth();
-        final Month currentMonth = currentDate.getMonth();
-        for (final Job job : jobs) {
-            final LocalDateTime jobDate = LocalDateTime.parse(job.getStartTime(), DATE_TIME_FORMAT);
-            final int jobDay = jobDate.getDayOfMonth();
-            final Month jobMonth = jobDate.getMonth();
-            //test if the job occurs within the coming month
-            if (jobMonth.equals(currentMonth) && jobDay >= currentDayOfMonth
-                    || jobMonth.plus(1).equals(currentMonth) && jobDay < currentDayOfMonth) {
-                //increment the position that is the number of days away from the current day
-                final int numOfDaysPastCurrentDay = (int) currentDate.until(jobDate, ChronoUnit.DAYS);
-                final int row = numOfDaysPastCurrentDay / numOfWeekRows;
-                final int col = numOfDaysPastCurrentDay % DAYS_IN_WEEK;
-                jobsForCalendarDays[row][col]++;
+    public String toString() {
+        final int[][] jobsByCalendarDays = createJobCalendar();
+        final StringBuilder returnString = new StringBuilder(512);
+        returnString.append(CALENDAR_HEADER);
+        returnString.append(createMonthHeader(currentDate.getMonth()));
+        LocalDateTime iterationDate = currentDate;
+        for (int row = 0; row < jobsByCalendarDays.length; row++) {
+            //start of the week
+            returnString.append("|");
+            for (int col = 0; col < DAYS_IN_WEEK; col++) {
+                //if the current day should not be displayed
+                if (jobsByCalendarDays[row][col] == NONDISPLAYED_DAY) {
+                	//NOTE that the date does not increment!
+                    returnString.append(EMPTY_DAY);
+                //if it should be displayed
+                } else {
+                    /*
+                     * If the next month has just been entered, we must break to the next row and
+                     * add the header for the next month.
+                     * Specifically, this checks if the day of month is 1 and if the month isn't the
+                     * same as the current date's
+                     */
+                    if (iterationDate.getDayOfMonth() == 1
+                            && !iterationDate.getMonth().equals(currentDate.getMonth())) {
+                        //fill in the remaining days in the row as empty
+                        for (int col2 = col; col2 < DAYS_IN_WEEK; col2++) {
+                            returnString.append(EMPTY_DAY);
+                        }
+                        returnString.append("\n");
+                        //insert header for next month. iterationDate currently is in next month
+                        returnString.append(createMonthHeader(iterationDate.getMonth()));
+                        returnString.append("|");
+                        //fill in enough days to align with the correct day of week
+                        for (int col2 = 0; col2 < col; col2++) {
+                            returnString.append(EMPTY_DAY);
+                        }
+                    }
+                    //insert " dd:j |" where 'dd' is the day of month and 'j' is jobs for the day
+                    returnString.append(String.format("%1$3d", iterationDate.getDayOfMonth())
+                        + ":" + jobsByCalendarDays[row][col] + " |");
+                    iterationDate = iterationDate.plusDays(1);
+                    //END IF-ELSE
+                }
+                //END FOR COLUMN
+            }
+            //if this is not the last row, add a line break
+            if (row < jobsByCalendarDays.length - 1) {
+                returnString.append("\n");
             }
         }
-        
-        //Shift values over by the number of prior days in the first week
-        final int currentDayOfWeekInt = currentDate.getDayOfWeek().getValue() - 1;
-        //shift the last row
-        for (int j = DAYS_IN_WEEK - currentDayOfWeekInt; j >= 0; j--) {
-        	jobsForCalendarDays[numOfWeekRows][j + currentDayOfWeekInt] = jobsForCalendarDays[numOfWeekRows][j];
-        }
-        //other rows
-        for (int i = numOfWeekRows - 2; i >= 0; i--) {
-        	for (int j = DAYS_IN_WEEK; j >= 0; j--) {
-        		if (j > DAYS_IN_WEEK - currentDayOfWeekInt) {
-        			jobsForCalendarDays[i + 1][(j + currentDayOfWeekInt) % DAYS_IN_WEEK] = jobsForCalendarDays[i][j];
-        		} else {
-        			jobsForCalendarDays[i][j + currentDayOfWeekInt] = jobsForCalendarDays[i][j];
-        		}
-        	}
-        }
-        
-        //set the prior days of the first week row as nondisplayed
-        for (DayOfWeek i = DayOfWeek.SUNDAY; i.compareTo(currentDate.getDayOfWeek()) < 0; i.plus(1)) {
-        	jobsForCalendarDays[0][i.getValue() - 1] = NONDISPLAYED_DAY;
-        }
-        //set the days at and after one month in the future as nondisplayed
-        for (DayOfWeek i = DayOfWeek.SATURDAY; i.compareTo(endDate.getDayOfWeek()) >= 0; i.minus(1)) {
-        	jobsForCalendarDays[numOfWeekRows - 1][i.getValue() - 1] = NONDISPLAYED_DAY;
-        }
-        return jobsForCalendarDays;
-	}
+        return returnString.toString();
+    }
 
+    /**
+     * Returns how many days since the last Sunday the given date is.
+     * @param theDate The given date
+     * @return How many days since the last Sunday the given date is
+     */
+    private int daysSinceSunday(final LocalDateTime theDate) {
+        //A DayOfWeek enum is Monday = 1, Sunday = 7
+        //This method makes it Sunday = 0, Saturday = 6
+        return theDate.getDayOfWeek().getValue() % 7;
+    }
+
+    /**
+     * Returns the date one month later.
+     * @param theDate The given date
+     * @return The date one month later than the given date.
+     */
+    private LocalDateTime monthLater(final LocalDateTime theDate) {
+        LocalDateTime monthLater = theDate.plusMonths(1);
+        /*
+         * If the given date's day of month is nonexistent in the next month, monthLater will be off
+         * by one day. This can be fixed by adding a day.
+         */
+        if (monthLater.getDayOfMonth() != theDate.getDayOfMonth()) {
+            monthLater = monthLater.plusDays(1);
+        }
+        return monthLater;
+    }
+
+    /**
+     * Finds the size of the calendar from the starting date to the ending date in rows.
+     * If the startDate is erroneously in future of the endDate, the calendar will be from
+     * end to start.
+     * @param theStartDate The starting date
+     * @param theEndDate The ending date
+     * @return The size of the calendar in rows
+     */
+    private int calendarSize(LocalDateTime startDate, LocalDateTime endDate) {
+        //If the startDate is after the endDate, swap the arguments
+        if (startDate.compareTo(endDate) > 0) {
+            final LocalDateTime tmp = startDate;
+            startDate = endDate;
+            endDate = tmp;
+        }
+        final int startDateDaysSinceSunday = daysSinceSunday(startDate);
+        //Addition is for the startDate itself!
+        final int totalDays = (int) startDate.until(endDate, ChronoUnit.DAYS) + 1;
+        //Addition is because we need at least one row!
+        final int calendarSize = (startDateDaysSinceSunday + totalDays - 1) / DAYS_IN_WEEK + 1;
+        return calendarSize;
+    }
+
+
+
+    /**
+     * Creates an array representing the calendar of jobs for the upcoming month. The columns are
+     * days of the week starting from Sunday. Each day contains the number of jobs occurring that
+     * day. If a day is not shown in this calendar, it is set as NONDISPLAYED_DAY.
+     * @return The calendar of jobs for the upcoming month.
+     */
+    private int[][] createJobCalendar() {
+        final int currentDateDaysSinceSunday = daysSinceSunday(currentDate);
+        final LocalDateTime monthLater = monthLater(currentDate);
+        final int calendarRows = calendarSize(currentDate, monthLater.minusDays(1));
+        final int[][] jobsByCalendarDays = new int[calendarRows][DAYS_IN_WEEK];
+        for (final Job job : jobs) {
+            final LocalDateTime jobDate = job.getStartTime();
+            /*
+             * If this job's date is between the current date (inclusive) and a month later
+             * (exclusive), then it is in our calendar.
+             */
+            if (jobDate.compareTo(currentDate) >= 0 && jobDate.compareTo(monthLater) < 0) {
+                final int jobDateCalendarPosition = currentDateDaysSinceSunday
+                                                    + (int) currentDate.until(jobDate, ChronoUnit.DAYS);
+                final int row = jobDateCalendarPosition / DAYS_IN_WEEK;
+                final int col = jobDateCalendarPosition % DAYS_IN_WEEK;
+                jobsByCalendarDays[row][col]++;
+            }
+        }
+        //mark the days before the current date as nondisplayed
+        for (int col = 0; col < currentDateDaysSinceSunday; col++) {
+            jobsByCalendarDays[0][col] = NONDISPLAYED_DAY;
+        }
+        //mark the days after and including one month later as nondisplayed
+        for (int col = DAYS_IN_WEEK - 1; col >= daysSinceSunday(monthLater); col--) {
+            jobsByCalendarDays[calendarRows - 1][col] = NONDISPLAYED_DAY;
+        }
+        return jobsByCalendarDays;
+    }
+
+    /**
+     * Creates the header for the given month.
+     * @param theMonth The given month
+     * @return The header for the given month
+     */
+    private String createMonthHeader(final Month theMonth) {
+        final String monthName = MONTH_NAMES[theMonth.getValue() - 1];
+        return MONTH_HEADER_START + monthName + MONTH_HEADER_END;
+    }
 }
--- src/test/CalendarTest.java
+++ src/test/CalendarTest.java
@@ -19,63 +19,80 @@ import model.Job;
  */
 public class CalendarTest {
 
-	private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-	
-	private static final List<Job> listOfJobs = new ArrayList<Job>();
-	
-	@BeforeClass
-	public static void init() {
-		final Job januaryJob = new Job("", null);
-		januaryJob.setStartTime("2017-01-13 13:00");
-		listOfJobs.add(januaryJob);
-		
-		final Job februaryJob = new Job("", null);
-		februaryJob.setStartTime("2017-02-03 08:00");
-		listOfJobs.add(februaryJob);
-	}
-	
-	@Test
-	public void testSingleMonthDisplay() {
-		final Calendar januaryCalendar = new Calendar(listOfJobs);
-		januaryCalendar.setCurrentDate(LocalDateTime.parse("2017-01-01 00:00", DATE_TIME_FORMAT));
-		final String expected = "   Su     M      T      W      T      F      S\n"
-				              + "               [January]\n"
-				              + "|  1:0 |  2:0 |  3:0 |  4:0 |  5:0 |  6:0 |  7:0 |\n"
-				              + "|  8:0 |  9:0 | 10:0 | 11:0 | 12:0 | 13:1 | 14:0 |\n"
-				              + "| 15:0 | 16:0 | 17:0 | 18:0 | 19:0 | 20:0 | 21:0 |\n"
-				              + "| 22:0 | 23:0 | 24:0 | 25:0 | 26:0 | 27:0 | 28:0 |\n"
-				              + "| 29:0 | 30:0 | 31:0 |      |      |      |      |";
-		assertEquals("Unable to correctly display a single month.", expected, januaryCalendar.toString());
-	}
-	
-	@Test
-	public void testDoubleMonthDisplay() {
-		final Calendar januaryFebruaryCalendar = new Calendar(listOfJobs);
-		januaryFebruaryCalendar.setCurrentDate(LocalDateTime.parse("2017-01-10 00:00", DATE_TIME_FORMAT));
-		final String expected = "   Su     M      T      W      T      F      S\n"
-				              + "               [January]\n"
+    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    
+    private static final List<Job> listOfJobs = new ArrayList<Job>();
+    
+    @BeforeClass
+    public static void init() {
+        final Job januaryJob = new Job("", null);
+        januaryJob.setStartTime("2017-01-13 13:00");
+        listOfJobs.add(januaryJob);
+        
+        final Job februaryJob = new Job("", null);
+        februaryJob.setStartTime("2017-02-03 08:00");
+        listOfJobs.add(februaryJob);
+    }
+    
+    @Test
+    public void testSingleMonthDisplay() {
+        final Calendar januaryCalendar = new Calendar(listOfJobs);
+        januaryCalendar.setCurrentDate(LocalDateTime.parse("2017-01-01 00:00", DATE_TIME_FORMAT));
+        final String expected = "   Su      M      T      W      T      F      S\n"
+                              + "               [January]\n"
+                              + "|  1:0 |  2:0 |  3:0 |  4:0 |  5:0 |  6:0 |  7:0 |\n"
+                              + "|  8:0 |  9:0 | 10:0 | 11:0 | 12:0 | 13:1 | 14:0 |\n"
+                              + "| 15:0 | 16:0 | 17:0 | 18:0 | 19:0 | 20:0 | 21:0 |\n"
+                              + "| 22:0 | 23:0 | 24:0 | 25:0 | 26:0 | 27:0 | 28:0 |\n"
+                              + "| 29:0 | 30:0 | 31:0 |      |      |      |      |";
+        assertEquals("Unable to correctly display a single month.", expected, januaryCalendar.toString());
+    }
+    
+    @Test
+    public void testDoubleMonthDisplay() {
+        final Calendar januaryFebruaryCalendar = new Calendar(listOfJobs);
+        januaryFebruaryCalendar.setCurrentDate(LocalDateTime.parse("2017-01-10 00:00", DATE_TIME_FORMAT));
+        final String expected = "   Su      M      T      W      T      F      S\n"
+                              + "               [January]\n"
                               + "|      |      | 10:0 | 11:0 | 12:0 | 13:1 | 14:0 |\n"
-				              + "| 15:0 | 16:0 | 17:0 | 18:0 | 19:0 | 20:0 | 21:0 |\n"
-				              + "| 22:0 | 23:0 | 24:0 | 25:0 | 26:0 | 27:0 | 28:0 |\n"
-				              + "| 29:0 | 30:0 | 31:0 |      |      |      |      |\n"
-				              + "               [February]\n"
-				              + "|      |      |      |  1:0 |  2:0 |  3:1 |  4:0 |\n"
-				              + "|  5:0 |  6:0 |  7:0 |  8:0 |  9:0 |      |      |";
-		assertEquals("Unable to correctly display two months.", expected, januaryFebruaryCalendar.toString());
-	}
-	
-	@Test
-	public void testLeapYearDisplay() {
-		final Calendar leapYearCalendar = new Calendar(listOfJobs);
-		leapYearCalendar.setCurrentDate(LocalDateTime.parse("2016-02-01 00:00", DATE_TIME_FORMAT));
-		final String expected = "   Su     M      T      W      T      F      S\n"
-				              + "               [February]\n"
-				              + "|      |  1:0 |  2:0 |  3:0 |  4:0 |  5:0 |  6:0 |\n"
-				              + "|  7:0 |  8:0 |  9:0 | 10:0 | 11:0 | 12:0 | 13:0 |\n"
-				              + "| 14:0 | 15:0 | 16:0 | 17:0 | 18:0 | 19:0 | 20:0 |\n"
-				              + "| 21:0 | 22:0 | 23:0 | 24:0 | 25:0 | 26:0 | 27:0 |\n"
-				              + "| 28:0 | 29:0 |      |      |      |      |      |";
-		assertEquals("Unable to correctly display February during a leap year.", expected, leapYearCalendar.toString());
-	}
+                              + "| 15:0 | 16:0 | 17:0 | 18:0 | 19:0 | 20:0 | 21:0 |\n"
+                              + "| 22:0 | 23:0 | 24:0 | 25:0 | 26:0 | 27:0 | 28:0 |\n"
+                              + "| 29:0 | 30:0 | 31:0 |      |      |      |      |\n"
+                              + "               [February]\n"
+                              + "|      |      |      |  1:0 |  2:0 |  3:1 |  4:0 |\n"
+                              + "|  5:0 |  6:0 |  7:0 |  8:0 |  9:0 |      |      |";
+        assertEquals("Unable to correctly display two months.", expected, januaryFebruaryCalendar.toString());
+    }
+    
+    @Test
+    public void testLeapYearDisplay() {
+        final Calendar leapYearCalendar = new Calendar(listOfJobs);
+        leapYearCalendar.setCurrentDate(LocalDateTime.parse("2016-02-01 00:00", DATE_TIME_FORMAT));
+        final String expected = "   Su      M      T      W      T      F      S\n"
+                              + "               [February]\n"
+                              + "|      |  1:0 |  2:0 |  3:0 |  4:0 |  5:0 |  6:0 |\n"
+                              + "|  7:0 |  8:0 |  9:0 | 10:0 | 11:0 | 12:0 | 13:0 |\n"
+                              + "| 14:0 | 15:0 | 16:0 | 17:0 | 18:0 | 19:0 | 20:0 |\n"
+                              + "| 21:0 | 22:0 | 23:0 | 24:0 | 25:0 | 26:0 | 27:0 |\n"
+                              + "| 28:0 | 29:0 |      |      |      |      |      |";
+        assertEquals("Unable to correctly display February during a leap year.", expected, leapYearCalendar.toString());
+    }
+    
+    @Test
+    public void testCurrentDayOfMonthDoesNotExistInNextMonth() {
+        final Calendar leapYearCalendar = new Calendar(listOfJobs);
+        leapYearCalendar.setCurrentDate(LocalDateTime.parse("2017-01-31 00:00", DATE_TIME_FORMAT));
+        final String expected = "   Su      M      T      W      T      F      S\n"
+                              + "               [January]\n"
+                              + "|      |      | 31:0 |      |      |      |      |\n"
+                              + "               [February]\n"
+                              + "|      |      |      |  1:0 |  2:0 |  3:1 |  4:0 |\n"
+                              + "|  5:0 |  6:0 |  7:0 |  8:0 |  9:0 | 10:0 | 11:0 |\n"
+                              + "| 12:0 | 13:0 | 14:0 | 15:0 | 16:0 | 17:0 | 18:0 |\n"
+                              + "| 19:0 | 20:0 | 21:0 | 22:0 | 23:0 | 24:0 | 25:0 |\n"
+                              + "| 26:0 | 27:0 | 28:0 |      |      |      |      |";
+        assertEquals("Unable to correctly handle when the current day of the month is nonexistent in the next month.",
+                     expected, leapYearCalendar.toString());
+    }
 
 }
