94a66fa3e9ce71ad7512728217da1558a3b0ed5a
Jasmine Dacones <jasminedacones@gmail.com>
2017-02-09 13:46:59 -0800

    Testing anchor links.

--- src/model/Job.java
+++ src/model/Job.java
@@ -15,7 +15,7 @@ import java.time.format.DateTimeFormatter;
  *
  */
 
-[TestLink](#test-link)
+[getVolunteers](#test-link)
 public class Job implements Serializable {
 	
 	private static final long serialVersionUID = 812488421841214L;
