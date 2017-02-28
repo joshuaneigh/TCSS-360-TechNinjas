f543a7f7644f3d3a1ecca21cf042fe0e5ee2188f
Youcef Bennour <ybennour@uw.edu>
2017-02-28 07:57:14 -0800

    Update TestBR2c.java

--- src/com/theTechNinjas/urbanParks/test/TestBR2c.java
+++ src/com/theTechNinjas/urbanParks/test/TestBR2c.java
@@ -16,20 +16,20 @@ import com.theTechNinjas.urbanParks.model.exception.ScheduleConflictException;
 public class TestBR2c {
 	
 	@Test (expected = ScheduleConflictException.class) 
-	public void checkWhetherTheNumberOfDaysExceedsMaximum() throws ScheduleConflictException{
+	public void addJob_checkWhetherTheNumberOfDaysExceedsMaximum() throws ScheduleConflictException{
 		String jobOne = "Visitor Center Volunteers\tSign Up the volunteers at the visitor Center\t2017-03-25 12:00\t2017-03-29 13:00";
 		Controller.addPark("Jarrell Cove State Park");
 		Controller.addJob("Jarrell Cove State Park", jobOne);
 	}
 	
-	public void checkWhetherTheNumberOfDaysEqualsMaximum() throws IllegalFormatException, ScheduleConflictException{
+	public void addJob_checkWhetherTheNumberOfDaysEqualsMaximum() throws IllegalFormatException, ScheduleConflictException{
 		String jobTwo = "Public Safety\tWatch over the public just in case\t2017-03-25 12:00\t2017-03-27 13:00";
 		Controller.addPark("Joemma Park");
 		Controller.addJob("Joemma Park", jobTwo);
 		
 	}
 	
-	public void checkWhetherTheNumberOfDaysLessThanMaximum() throws IllegalFormatException, ScheduleConflictException{
+	public void addJob_checkWhetherTheNumberOfDaysLessThanMaximum() throws IllegalFormatException, ScheduleConflictException{
 		String jobThree = "Public Safety\tWatch over the public just in case\t2017-03-25 12:00\t2017-03-25 13:00";
 		Controller.addPark("Beach Park");
 		Controller.addJob("Beach Park", jobThree);
@@ -37,7 +37,7 @@ public class TestBR2c {
 	}
 	
 	@Test (expected = IllegalFormatException.class) 
-	public void checkWhetherTheNumberOfDaysLessThanTheCurrentDay() throws IllegalFormatException, ScheduleConflictException{
+	public void addJob_checkWhetherTheNumberOfDaysLessThanTheCurrentDay() throws IllegalFormatException, ScheduleConflictException{
 		String jobFour = "Public Safety\tWatch over the public just in case\t2017-03-25 12:00\t2017-03-24 13:00";
 		Controller.addPark("Park");
 		Controller.addJob("Park", jobFour);
